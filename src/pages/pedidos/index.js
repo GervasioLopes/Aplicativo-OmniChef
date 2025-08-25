f1ae4f3fc3e69c3f17ca13e4ea9e4d3444aed4c4
Bruno Santos Marques <bruno_ssmarqes@hotmail.com>
2025-08-24 23:11:41 -0300

    Inserindo uma arrays com uma lista de objetos (itens do cardapio) e uma array que servira como carrinho de compra

---
+++ src/pages/pedidos/index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // ---- DADOS DOS ITENS DO MENU ----
+    const menuItems = [
+        { id: 1, name: 'FRANGO A PASSARINHO', price: 25.00, prepTime: 20, image: '/Aplicativo_OmniChef/public/assets/images/Imagens/Frango-a-passarinho.jpg' },
+        { id: 2, name: 'BATATA FRITA C/ CHEDDAR', price: 25.00, prepTime: 10, image: 'https://i.imgur.com/2sS9d5G.jpeg' },
+        { id: 3, name: 'CALABRESA ACEBOLADA', price: 30.00, prepTime: 10, image: 'https://i.imgur.com/KxV0C3P.jpeg' },
+        { id: 4, name: 'DADINHO DE TAPIOCA', price: 25.00, prepTime: 10, image: 'https://i.imgur.com/X4Jv7hJ.jpeg' },
+        { id: 5, name: 'COXINHA 8 UNIDS', price: 20.00, prepTime: 15, image: 'https://i.imgur.com/XQj3sYx.jpeg' },
+        { id: 6, name: 'KIBE 8 UNIDS', price: 35.00, prepTime: 18, image: 'https://i.imgur.com/J8b0s5Z.jpeg' },
+        { id: 7, name: 'ONION RINGS', price: 25.00, prepTime: 10, image: 'https://i.imgur.com/nJgqL6k.jpeg' },
+        { id: 8, name: 'PASTEL 6 UNIDS', price: 25.00, prepTime: 15, image: 'https://i.imgur.com/yO8Gz3p.jpeg' },
+    ];
+
+    // ---- ESTADO DO PEDIDO ATUAL ----
+    let currentOrder = []; /*Uma arrays vazia que serve para guardar os itens selecionados pelo cliente ou seja, e um carrinho de compra*/ 
+
+
+
+
+});
